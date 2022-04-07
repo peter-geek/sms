316ae7af44d64c3de6fa6819efddc7565a6f9c17
Peter <coderscave1@gmail.com>
2022-04-07 23:46:48 +0300

    Adding user to database

--- files/dashboard.php
+++ files/dashboard.php
@@ -17,8 +17,10 @@
                     <div class="flex-row flex">
                         <div class="flex-shrink-0" style="width: 300px;">
                             <form class="user" action="<?= $_SERVER['REQUEST_URI'] ?>" method="POST">
-                                <p class="label">Full name</p>
-                                <input type="text" name="name" placeholder="e.g Mukiibi Peter" required>
+                                <p class="label">First name</p>
+                                <input type="text" name="fname" placeholder="e.g Peter" required>
+                                <p class="label">Last name</p>
+                                <input type="text" name="lname" placeholder="e.g Mukiibi" required>
                                 <p class="label">Email address</p>
                                 <input type="email" name="email" placeholder="e.g admin@example.com" required>
                                 <p class="label">Account type</p>
@@ -31,6 +33,26 @@
                                 <input type="password" name="password" placeholder="e.g 87T%^RF" required>
                                 <input type="hidden" name="user_id" value="0">
                                 <button type="submit">submit</button>
+                                <div>
+                                    <?php
+                                    if (isset($_POST['fname'])) {
+                                        $fname = $_POST['fname'];
+                                        $lname = $_POST['lname'];
+                                        $email = $_POST['email'];
+                                        $acc_type = $_POST['acc_type'];
+                                        $password = $_POST['password'];
+                                        $user_id = $_POST['user_id'];
+                                        // name email password acc_type
+                                        $encrypted_pass = password_hash($password, PASSWORD_DEFAULT);
+
+                                        $sql = "INSERT INTO user (`fname`, `lname`, `email`, `password`, `acc_type`) values('$fname', '$lname', '$email', '$encrypted_pass', '$acc_type')";
+                                        if ($user_id > 0) {
+                                            $sql = "UPDATE user set `fname` = '$fname', `lname` = '$lname', `email` = '$email', `password` = '$encrypted_pass', `acc_type` where id = '$user_id'";
+                                        }
+                                        $db->query($sql) or die($db->error);
+                                    }
+                                    ?>
+                                </div>
                             </form>
                         </div>
                         <div class="flex-grow">
@@ -43,17 +65,24 @@
                                     <th style="width: 10px;"></th>
                                     <th style="width: 10px;"></th>
                                 </tr>
-                                <tr>
-                                    <td>MUKIIBI PETER</td>
-                                    <td>admin@example.com</td>
-                                    <td>Student</td>
-                                    <td>
-                                        <a href="#" class="ic ic_rounded ic_green f-jc-ic"><?= $svg['edit'] ?></a>
-                                    </td>
-                                    <td>
-                                        <a href="#" class="ic ic_rounded ic_red f-jc-ic"><?= $svg['delete'] ?></a>
-                                    </td>
-                                </tr>
+                                <?php
+                                $q = $db->query("SELECT * FROM user order by fname asc");
+                                while ($d = $q->fetch_assoc()) {
+                                ?>
+                                    <tr>
+                                        <td><?= $d['fname'] . ' ' . $d['lname'] ?></td>
+                                        <td><?= $d['email'] ?></td>
+                                        <td><?= strtolower($d['acc_type']) ?></td>
+                                        <td>
+                                            <a href="#" class="ic ic_rounded ic_green f-jc-ic"><?= $svg['edit'] ?></a>
+                                        </td>
+                                        <td>
+                                            <a href="#" class="ic ic_rounded ic_red f-jc-ic"><?= $svg['delete'] ?></a>
+                                        </td>
+                                    </tr>
+                                <?php
+                                }
+                                ?>
                             </table>
                         </div>
                     </div>
